a9dfdc59b06e518f5a287f204b51244452d32fa9
3bicik <trzebinski.dawid.98@gmail.com>
2021-12-31 02:51:43 +0100

    delete old idea

--- backend/wwzd_api/wwzd/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Character, Line
-
-admin.site.register(Character)
-admin.site.register(Line)
--- backend/wwzd_api/wwzd/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WwzdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wwzd'
