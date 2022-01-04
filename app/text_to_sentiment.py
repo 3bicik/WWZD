0d60c41b3ecc23a8cc2c760f0a8b2f98911d1185
3bicik <trzebinski.dawid.98@gmail.com>
2022-01-04 10:37:05 +0100

    changed vector format to [x,y]

--- app/text_to_sentiment.py
+++ app/text_to_sentiment.py
@@ -19,7 +19,7 @@ classes = ["Happy", "Sad", "Love", "Anger"]
 
 def prep_lines_dataframe():
     lines = pandas.read_csv('data/simpsons_dataset.csv')
-    sentiment = numpy.zeros((len(lines),4))
+    sentiment = numpy.zeros((len(lines),2))
     lines['sentiment'] = sentiment.tolist()
     lines['raw_character_text'] = lines['raw_character_text'].astype(str)
     lines['spoken_words'] = lines['spoken_words'].astype(str)
@@ -39,28 +39,9 @@ def process_data(lines):
     return model.predict(data_test, verbose='1')
 
 
-def calculate_averages(lines, threshold):
-    name_temp = ''
-    vec_temp = numpy.zeros(len(lines['sentiment']))
-    times = 0
-
-    for index, row in lines.sort_values(by=['raw_character_text']).iterrows():
-        if(index == 0):
-            name_temp = row['raw_character_text']
-        
-        if(row['raw_character_text'] == name_temp):
-            vec_temp += row['sentiment']
-            times += 1
-        else:
-            if(times >= threshold):
-                Character(name=name_temp, sentiment=vec_temp/times, number_of_lines=times).save()
-            name_temp = row['raw_character_text']
-            vec_temp = row['sentiment']
-            times = 1
-
 def calculate_personality(lines, threshold):
     name_temp = ''
-    vec_temp = numpy.zeros(len(lines['sentiment']))
+    vec_temp = numpy.zeros(2)
     times = 0
 
     for index, row in lines.sort_values(by=['raw_character_text']).iterrows():
@@ -70,17 +51,35 @@ def calculate_personality(lines, threshold):
         if(row['raw_character_text'] == name_temp):
             pred = row['sentiment'].argmax(axis=-1)
             pred_prob = row['sentiment'][pred]
-            vec_temp[pred] += pred_prob**2
+
+            if(pred==0):
+                vec_temp[0] += pred_prob**2
+            if(pred==1):
+                vec_temp[0] -= pred_prob**2
+            if(pred==2):
+                vec_temp[1] += pred_prob**2
+            if(pred==3):
+                vec_temp[1] -= pred_prob**2
+
             # vec_temp += row['sentiment']
             times += 1
         else:
             if(times >= threshold):
                 Character(name=name_temp, sentiment=vec_temp/times, number_of_lines=times).save()
             name_temp = row['raw_character_text']
-            vec_temp = numpy.zeros(4)
+            vec_temp = numpy.zeros(2)
             pred = row['sentiment'].argmax(axis=-1)
             pred_prob = row['sentiment'][pred]
-            vec_temp[pred] += pred_prob**2
+            
+            if(pred==0):
+                vec_temp[0] += pred_prob**2
+            if(pred==1):
+                vec_temp[0] -= pred_prob**2
+            if(pred==2):
+                vec_temp[1] += pred_prob**2
+            if(pred==3):
+                vec_temp[1] -= pred_prob**2
+
             # vec_temp = row['sentiment']
             times = 1
 
